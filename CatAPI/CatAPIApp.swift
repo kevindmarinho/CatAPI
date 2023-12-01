52acb39199148645fa31fb581d637e62765ef9e9
kevin marinho <kevindmarinho4321@gmail.com>
2023-11-30 21:04:44 -0400

    Initial Commit

---
+++ CatAPI/CatAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatAPIApp.swift
+//  CatAPI
+//
+//  Created by kevin marinho on 30/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CatAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
